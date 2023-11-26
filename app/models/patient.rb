d1775523999487df54610a1c899ea12233343e54
Mateusz Jankowski <82528259+jankowski-mateusz@users.noreply.github.com>
2023-11-26 18:04:19 +0100

    Import patients from csv (#2)

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,18 +1,17 @@
-class Patient < ApplicationRecord
-  include Activepesel::PeselAttr
-  pesel_attr :pesel
+# frozen_string_literal: true
 
+class Patient < ApplicationRecord
   enum gender: { male: 1, female: 2 }
 
-  validates :first_name, :last_name, :city, presence: true
-  validates :pesel, pesel: true, uniqueness: true
+  validates :first_name, :last_name, :city, :pesel, presence: true
+  validates :pesel, uniqueness: true
 
   before_save :set_personal_data
 
   private
 
   def set_personal_data
-    self.date_of_birth = pesel_personal_data.date_of_birth
-    self.gender = pesel_personal_data.sex
+    self.date_of_birth = BirthdateFromPesel::Parser.new(pesel).birthdate
+    self.gender = pesel[9].to_i.even? ? 2 : 1
   end
 end

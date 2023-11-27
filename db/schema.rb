9a74d0299a818552ff5cab54a5e228e6be425f3d
Mateusz Jankowski <82528259+jankowski-mateusz@users.noreply.github.com>
2023-11-27 09:11:09 +0100

    Add doctor model (#5)

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_26_154059) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_27_080829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name", null: false

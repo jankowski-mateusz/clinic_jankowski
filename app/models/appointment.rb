c88607544e4ed526e2b75dd049ae388ec3524581
Mateusz Jankowski <mateusz.jankowski213@gmail.com>
2023-11-27 15:46:12 +0100

    fix eager loading and add appointents index

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Appointment < ApplicationRecord
-  belongs_to :patient
+  belongs_to :patient, counter_cache: :appointments_count
   belongs_to :doctor
 
   def self.ransackable_attributes(auth_object = nil)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_082233) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_27_142941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_082233) do
     t.date "date_of_birth"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "appointments_count", default: 0
     t.index ["pesel"], name: "index_patients_on_pesel", unique: true
   end
 
